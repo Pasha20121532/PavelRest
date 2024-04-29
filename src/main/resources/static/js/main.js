0beb1e6091a1a3487b7e89613e81c1d745f0dd20
Pasha20121532 <Ipashrum@gmail.com>
2024-04-29 19:52:26 +0300

    Мурашев Лаб 3 Корп защ

---
+++ src/main/resources/static/js/main.js
@@ -0,0 +1,29 @@
+var messageApi = Vue.resource('/message{id}')
+
+Vue.component('message-row',{
+    props: ['message'],
+    template: '<div><i>({{message.id}})</i> {{message.text}}</div>'
+
+});
+
+Vue.component('message-list',{
+    props: ['messages'],
+    template: '<div><message-row v-for="message in messages"' +
+        'v-bind:key="message.id" :message="message" /></div>',
+    created: function ()
+    {
+        messageApi.get().then(result =>
+            result.json().then(data =>
+            data.forEach(message => this.messages.push(message))
+            )
+        )
+    }
+});
+
+var app = new Vue({
+    el: '#app',
+    template: '<message-list :messages="messages"/> ',
+    data: {
+        messages: []
+    }
+});
