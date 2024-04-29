aec47f6fa7d4ee4e501aef770d032a86052a187f
Pasha20121532 <Ipashrum@gmail.com>
2024-04-29 18:58:39 +0300

    REST приложение Мурашев

---
+++ src/main/java/com/Murashev/PavelRest/PavelRestApplication.java
@@ -0,0 +1,13 @@
+package com.Murashev.PavelRest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PavelRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PavelRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Murashev/PavelRest/PavelRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Murashev.PavelRest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PavelRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
